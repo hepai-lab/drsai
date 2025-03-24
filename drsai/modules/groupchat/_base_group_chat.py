d0e0e663b703df3152e5676023829535ab706f1b
xdb@mail.ustc.edu.cn <xdb@mail.ustc.edu.cn>
2025-03-24 17:35:54 +0800

    update groupchat

--- drsai/modules/groupchat/_base_group_chat.py
+++ drsai/modules/groupchat/_base_group_chat.py
@@ -7,11 +7,12 @@ from typing import Any, AsyncGenerator, List, Sequence, Callable
 from autogen_core import (
     AgentId,
     CancellationToken,
+    AgentRuntime
 )
 
 from autogen_agentchat.base import ChatAgent, TaskResult, TerminationCondition
 from autogen_agentchat.messages import AgentEvent, BaseChatMessage, ChatMessage, ModelClientStreamingChunkEvent, TextMessage
-from autogen_agentchat.teams._group_chat._events import GroupChatStart
+from autogen_agentchat.teams._group_chat._events import GroupChatStart, GroupChatTermination
 from autogen_agentchat.teams._group_chat._sequential_routed_agent import SequentialRoutedAgent
 from autogen_agentchat.teams._group_chat._base_group_chat_manager import BaseGroupChatManager
 from autogen_agentchat.teams import BaseGroupChat
@@ -31,20 +32,26 @@ class DrSaiGroupChatManager(BaseGroupChatManager):
         group_topic_type: str,
         output_topic_type: str,
         participant_topic_types: List[str],
+        participant_names: List[str],
         participant_descriptions: List[str],
+        output_message_queue: asyncio.Queue[AgentEvent | ChatMessage | GroupChatTermination],
         termination_condition: TerminationCondition | None = None,
         max_turns: int | None = None,
         thread: Thread = None,
         thread_mgr: ThreadsManager = None,
+        **kwargs: Any
     ):
+        
         super().__init__(
-            name = name,
-            group_topic_type = group_topic_type,
-            output_topic_type = output_topic_type,
-            participant_topic_types = participant_topic_types,
-            participant_descriptions = participant_descriptions,
-            termination_condition = termination_condition,
-            max_turns = max_turns
+            name=name,
+            group_topic_type=group_topic_type,
+            output_topic_type=output_topic_type,
+            participant_topic_types=participant_topic_types,
+            participant_names=participant_names,
+            participant_descriptions=participant_descriptions,
+            output_message_queue=output_message_queue,
+            termination_condition=termination_condition,
+            max_turns=max_turns,
         )
         self._theard: Thread = thread
         self._thread_mgr: ThreadsManager = thread_mgr
@@ -58,40 +65,49 @@ class DrSaiGroupChat(BaseGroupChat):
     def __init__(
         self,
         participants: List[ChatAgent],
+        group_chat_manager_name: str,
         group_chat_manager_class: type[SequentialRoutedAgent],
         termination_condition: TerminationCondition | None = None,
         max_turns: int | None = None,
+        runtime: AgentRuntime | None = None,
         thread: Thread = None,
         thread_mgr: ThreadsManager = None,
         **kwargs: Any
     ):
         super().__init__(
-            participants = participants,
-            group_chat_manager_name = "DrSaiGroupChatManager",
-            group_chat_manager_class = group_chat_manager_class, 
-            termination_condition = termination_condition, 
-            max_turns = max_turns
+            participants=participants,
+            group_chat_manager_name=group_chat_manager_name,
+            group_chat_manager_class=group_chat_manager_class,
+            termination_condition=termination_condition,
+            max_turns=max_turns,
+            runtime=runtime,
             )
         self._thread: Thread = thread
         self._thread_mgr: ThreadsManager = thread_mgr
 
     def _create_group_chat_manager_factory(
-        self,
+       self,
+        name: str,
         group_topic_type: str,
         output_topic_type: str,
         participant_topic_types: List[str],
+        participant_names: List[str],
         participant_descriptions: List[str],
+        output_message_queue: asyncio.Queue[AgentEvent | ChatMessage | GroupChatTermination],
         termination_condition: TerminationCondition | None,
         max_turns: int | None,
     ) -> Callable[[], DrSaiGroupChatManager]:
         def _factory() -> DrSaiGroupChatManager:
             return DrSaiGroupChatManager(
-                group_topic_type,
-                output_topic_type,
-                participant_topic_types,
-                participant_descriptions,
-                termination_condition,
-                max_turns,
+                name = name,
+                group_topic_type = group_topic_type,
+                output_topic_type = output_topic_type,
+                participant_topic_types = participant_topic_types,
+                participant_names = participant_names,
+                participant_descriptions = participant_descriptions,
+                output_message_queue = output_message_queue,
+                termination_condition = termination_condition,
+                max_turns = max_turns,
                 thread=self._thread,
                 thread_mgr=self._thread_mgr,
             )
