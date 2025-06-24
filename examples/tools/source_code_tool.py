408429d2b3b3efcaed2202b95a3944a1307b65c4
we12306 <xdb@mail.ustc.edu.cn>
2025-06-24 20:55:21 +0800

    add a_load_source_code_tools

---
+++ examples/tools/source_code_tool.py
@@ -0,0 +1,38 @@
+from autogen_core.tools._function_tool import FunctionTool, FunctionToolConfig
+from autogen_core.tools import StaticWorkbench
+from autogen_core.code_executor._func_with_reqs import (
+    Import, 
+    import_to_str, 
+    to_code,
+    ImportFromModule, 
+    Alias
+    )
+import asyncio
+async def main():
+    tool = [
+            {
+              "provider": "autogen_core.tools.FunctionTool",
+              "component_type": "tool",
+              "version": 1,
+              "component_version": 1,
+              "description": "Create custom tools by wrapping standard Python functions.",
+              "label": "FunctionTool",
+              "config": {
+                "source_code": "def calculator(a: float, b: float, operator: str) -> str:\n    try:\n        if operator == \"+\":\n            return str(a + b)\n        elif operator == \"-\":\n            return str(a - b)\n        elif operator == \"*\":\n            return str(a * b)\n        elif operator == \"/\":\n            if b == 0:\n                return \"Error: Division by zero\"\n            return str(a / b)\n        else:\n            return \"Error: Invalid operator. Please use +, -, *, or /\"\n    except Exception as e:\n        return f\"Error: {str(e)}\"\n",
+                "name": "calculator",
+                "description": "A simple calculator that performs basic arithmetic operations",
+                "global_imports": [ImportFromModule(module = "math", imports = ["pi"])],
+                "has_cancellation_support": False
+              }
+            }
+          ]
+    tool = FunctionTool._from_config(FunctionToolConfig(**tool[0]['config']))
+    Workbench = StaticWorkbench(tools = [tool])
+    result = await Workbench.call_tool(name = "calculator", arguments = {"a": 2, "b": 3, "operator": "*"})
+    print(result)
+
+    # MagenticAgent.load_component()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+    pass
--- src/drsai/agent_factory/load_agent.py
+++ src/drsai/agent_factory/load_agent.py
@@ -6,6 +6,7 @@ from typing import Union, Any, Dict, List, Optional, Callable
 from autogen_core.models import ChatCompletionClient, LLMMessage
 from autogen_core import ComponentModel, CancellationToken
 
+from autogen_core.tools._function_tool import FunctionTool, FunctionToolConfig
 from autogen_agentchat.agents._assistant_agent import AssistantAgentConfig
 from autogen_agentchat.agents import UserProxyAgent, BaseChatAgent
 from autogen_agentchat.teams import BaseGroupChat
@@ -16,7 +17,14 @@ from autogen_ext.tools.mcp import (
     SseMcpToolAdapter,
     StdioMcpToolAdapter,
     )
-
+from autogen_core.tools._function_tool import FunctionTool, FunctionToolConfig
+from autogen_core.code_executor._func_with_reqs import (
+    Import, 
+    import_to_str, 
+    to_code,
+    ImportFromModule, 
+    Alias
+    )
 from drsai import (
     AssistantAgent, 
     DrSaiRoundRobinGroupChat, 
@@ -207,6 +215,22 @@ def load_agent_factory_from_config(
 async def a_load_mcp_tools(mcp_tools_config: list[dict]) -> list[StdioMcpToolAdapter | SseMcpToolAdapter]:
     '''
     加载AutoGen的MCP工具
+    yaml:
+        mcp_tools_1: &tool_1
+            type: std
+            command: python3
+            args:
+                - /your/path/to/mcp_server.py
+            env: null
+
+            mcp_tools_2: &tool_2
+            type: sse
+            url: www.example.com
+            headers:
+                Authorization: Bearer <token>
+                event_name: mcp_tools_event
+            timeout: 20
+            sse_read_timeout: 300
     '''
     mcp_tools = []
     for mcp_tool_config in mcp_tools_config:
@@ -244,6 +268,14 @@ async def a_load_mcp_tools(mcp_tools_config: list[dict]) -> list[StdioMcpToolAda
 async def a_load_hepai_tools(hepai_tools_config: list[dict]) -> list[Callable]:
     '''
     加载HepAI的worker工具
+    yaml:
+        hepai_tools_1: &hepai_tool_1
+            worker_name: besiii_boss_08
+            api_key: sk-****
+            allowed_tools:
+                - json_mapping
+                - job_status
+            base_url: "https://aiapi.ihep.ac.cn/apiv2"
     '''
     hepai_tools = []
     try:
@@ -260,6 +292,56 @@ async def a_load_hepai_tools(hepai_tools_config: list[dict]) -> list[Callable]:
         return hepai_tools
     except ImportError:
         raise ImportError("please install <hepai> package")
+
+async def a_load_source_code_tools(source_tools_config: list[dict]) -> list[FunctionTool]:
+    '''
+    加载前端源代码工具
+    yaml:
+        source_tools_1: &source_tool_1
+            type: FunctionTool
+            config:
+                source_code: |
+                    def calculator(a: float, b: float, operator: str) -> str:
+                        try:
+                            if operator == "+":
+                                return str(a + b)
+                            elif operator == "-":
+                                return str(a - b)
+                            elif operator == "*":
+                                return str(a * b)
+                            elif operator == "/":
+                                if b == 0:
+                                    return "Error: Division by zero"
+                                return str(a / b)
+                            else:
+                                return "Error: Invalid operator. Please use +, -, *, or /"
+                        except Exception as e:
+                            return f"Error: {str(e)}"
+                name: calculator
+                description: A simple calculator that performs basic arithmetic operations
+                global_imports:
+                    - math: 
+                        - pi
+                        - e
+                has_cancellation_support: False
+    '''
+    source_code_tools = []
+    for source_tool_config in source_tools_config:
+        if source_tool_config["type"] == "FunctionTool":
+            global_imports = source_tool_config["config"].get("global_imports", [])
+            global_imports_modules = []
+            if global_imports:
+                for global_import in global_imports:
+                    module_name = list(global_import.keys())[0]
+                    import_list = global_import[module_name]
+                    global_imports_modules.append(ImportFromModule(module=module_name, imports=import_list))
+                source_tool_config["config"]["global_imports"] = global_imports_modules
+            tool_config = FunctionToolConfig(**source_tool_config["config"])
+            tool = FunctionTool._from_config(tool_config)
+            source_code_tools.append(tool)
+        else:
+            raise ValueError(f"source_tools的type只能是FunctionTool")
+    return source_code_tools
 
 async def a_load_memory_functions(memory_functions_config: dict) -> Callable:
     '''
@@ -324,6 +406,7 @@ async def a_load_agent_factory_from_config(
                 # 加载tools
                 mcp_tools = await a_load_mcp_tools(value.get("mcp_tools", []))
                 hepai_tools = await a_load_hepai_tools(value.get("hepai_tools", []))
+                source_tools = await a_load_source_code_tools(value.get("source_tools", []))
                 # TODO: 加载 memory_functions, RAG
                 if mode == "ui":
                     assistant_list.append(MagenticAgent(
@@ -331,7 +414,7 @@ async def a_load_agent_factory_from_config(
                         system_message=system_message,
                         description=description,
                         model_client=model_client,
-                        tools=mcp_tools+hepai_tools if len(mcp_tools+hepai_tools) > 0 else None,
+                        tools=mcp_tools+hepai_tools+source_tools if len(mcp_tools+hepai_tools+source_tools) > 0 else None,
                     ))
                 else:
                     assistant_list.append(AssistantAgent(
