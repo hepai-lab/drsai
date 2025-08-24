39831d23a8a8f8bcff0e834d5f7e0712b0789f04
xdb@mail.ustc.edu.cn <xdb@mail.ustc.edu.cn>
2025-08-25 00:05:38 +0800

    update readme

--- examples/cli/run_backend.py
+++ examples/cli/run_backend.py
@@ -1,6 +1,5 @@
-from drsai_ui.run_ui import ui
+from drsai_ui.run_ui import backend
 
 if __name__ == "__main__":
-    ui(
-        # reload=True,
-    )
+    agent_config = "agent_config.yaml"
+    backend(agent_config=agent_config)
---
+++ examples/cli/run_ui.py
@@ -0,0 +1,6 @@
+from drsai_ui.run_ui import ui
+
+if __name__ == "__main__":
+    ui(
+        # reload=True,
+    )
