86db701834472067b37faf319dc5a3118fdd6e6e
zdzhang@ihep.ac.cn <zdzhang@ihep.ac.cn>
2026-07-18 14:13:57 +0800

    release: unify version 1.4.8 and refresh manuals

--- apps/webui/backend/src/drsai_ui/ui_backend/version.py
+++ apps/webui/backend/src/drsai_ui/ui_backend/version.py
@@ -1,3 +1,3 @@
-VERSION = "1.4.6"
-__version__ = "1.4.6"
+VERSION = "1.4.8"
+__version__ = "1.4.8"
 APP_NAME = "Dr.Sai-UI"
--- cores/python/packages/drsai/src/drsai/version.py
+++ cores/python/packages/drsai/src/drsai/version.py
@@ -1,5 +1,5 @@
 
-__version__ = "1.4.7-rc1"
+__version__ = "1.4.8"
 __appname__ = 'drsai'
 __author__ = 'hepai'
 __email__ = 'xiongdb@ihep.ac.cn/hepai@ihep.ac.cn'
